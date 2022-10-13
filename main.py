bc5099606f666dbfc451e56915f8f0f8aa599c3f
Egorka <eshik2002@gmail.com>
2022-10-13 20:07:08 +0300

    Wrote check_point func

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+check_list = []
 
 
+# Декоратор check_point, с его помощью регистрируем любые нужные нам методы, как методы проверки города от пользователя
 def check_point(fun):
+    check_list.append(fun)
     return fun
 
 
 def normalize_city_name(text):
     return ""
+
 
 def is_city_startswith_char(city, char):
     return False
